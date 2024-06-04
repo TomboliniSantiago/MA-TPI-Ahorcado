da34e20cc54229a70181dc081fd5c04b161910fe
TomboliniSantiago <santitombo@gmail.com>
2024-06-03 22:18:08 -0300

    Se agrega Test que comprueba que funcione para palabras con letras en mayusculas

--- palabra.test.js
+++ palabra.test.js
@@ -9,6 +9,14 @@ describe('Validar Palabra', () => {
     test('La palabra no debe contener espacios', () => {
         expect(palabra('palabra_sin_espacios')).toBe(true);
     });
+});
 
+describe('Ocultar Palabra', () => {
+    test('Mostrar la palabra seleccionada como una serie de guiones bajos', () => {
+        expect(ocultarPalabra('palabra')).toBe('_______');
+    });
 
+    test('Ocultar palabra con letras mayúsculas', () => {
+        expect(ocultarPalabra('PALABRA')).toBe('_______');
+    });
 });
